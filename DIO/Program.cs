84e8bf4f6c2da2eece4127dc1eb542d8c327e599
Phelipe <phelipe.silva@sptech.school>
2025-04-08 23:56:54 -0300

    Adicionando diretórios na branch main

---
+++ DIO/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int qtdVezes = 5;
+for (int i = 0; i < qtdVezes; i++)
+{
+    Console.WriteLine($"Bem vindo ao {i + 1}º curso de .NET");
+}
